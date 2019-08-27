11422aafa965901e393535f3d77e211445f43580
Benjamin Taylor <drtayloratuwe@gmail.com>
2019-08-27 22:12:37 +0100

    update to repo

---
+++ titanicRandomForest.R
@@ -0,0 +1,6 @@
+setwd("/home/system/Documents/titanic")
+rm(list = ls())
+
+titanic.train <- read.csv(file = "train.csv", stringsAsFactors = FALSE, header = TRUE)
+titanic.test <- read.csv(file = "test.csv", stringsAsFactors = FALSE, header = TRUE)
+
